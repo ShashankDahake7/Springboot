923c9f970d096be543784174a52ef1e3b4235c3e
Shashank Dahake <119002734+ShashankDahake7@users.noreply.github.com>
2025-03-07 02:01:08 +0530

    JDBC

---
+++ Springboot/springbootJDBC/src/main/java/com/example/springbootJDBC/SpringbootJdbcApplication.java
@@ -0,0 +1,11 @@
+package com.example.springbootJDBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJdbcApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJdbcApplication.class, args);
+	}
+}
